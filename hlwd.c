5844e2bb9d5b18b15ab0b78a76045cc9d65fb7ca
Judge_Dredd <the.guard@mail.ru>
2013-09-23 14:15:27 +0400

    initial commit on branch for editing opening \Device\Video0 and getting resources

--- hlwd.c
+++ hlwd.c
@@ -7,15 +7,24 @@ NTSTATUS Close(PDEVICE_OBJECT, PIRP);
 NTSTATUS NotImplemented(PDEVICE_OBJECT, PIRP);
 void Dtor(PDRIVER_OBJECT );
 
+PVOID vaddr = 0;
+int FBSz = 0;
+
 NTSTATUS DriverEntry(PDRIVER_OBJECT pDriverObject, PUNICODE_STRING pRegistryPath){
   NTSTATUS NtStatus = STATUS_SUCCESS;
-  PDEVICE_OBJECT pDeviceObject = NULL;
-  UNICODE_STRING usDriverName, usDosDeviceName;
-  int i;
+  PDEVICE_OBJECT pDeviceObject = NULL, pVideoDeviceObject = NULL;
+  UNICODE_STRING usDriverName, usDosDeviceName, usVideoDeviceName;
+  PCM_RESOURCE_LIST pVideoDevResList;
+  PCM_FULL_RESOURCE_DESCRIPTOR pFullResourceDescriptor;
+  CM_PARTIAL_RESOURCE_LIST PartialResourceList;
+  PCM_PARTIAL_RESOURCE_DESCRIPTOR pPartialResourceDescriptor;
+  FILE_OBJECT FileObject;
+  int i, j, ResourceListSz = 3*sizeof(CM_RESOURCE_LIST), ResourceListNeededSz;
+  PHYSICAL_ADDRESS paddr;
 
+  DbgPrint("Driver Entry \n");
   RtlInitUnicodeString(&usDriverName, L"\\Device\\ElectricEye");
   RtlInitUnicodeString(&usDosDeviceName, L"\\DosDevices\\ElectricEye");
-  DbgPrint("Driver Entry \n");
   NtStatus = IoCreateDevice(pDriverObject, 0,
 			    &usDriverName,
 			    FILE_DEVICE_UNKNOWN,
@@ -30,19 +39,84 @@ NTSTATUS DriverEntry(PDRIVER_OBJECT pDriverObject, PUNICODE_STRING pRegistryPath
   pDriverObject->DriverUnload = &Dtor;
   pDeviceObject->Flags |= DO_DIRECT_IO;
   pDeviceObject->Flags &= (~DO_DEVICE_INITIALIZING);
+
+  /* Guess number one: video card device name */
+  RtlInitUnicodeString(&usVideoDeviceName, L"\\Device\\Video0");
+  if( !NT_SUCCESS(IoGetDeviceObjectPointer(&usVideoDeviceName, FILE_READ_DATA,
+				&FileObject, pVideoDeviceObject))){
+    DbgPrint("couldn't get device object pointer \n");
+    return  STATUS_UNSUCCESSFUL;
+  }
+  
+  if( (pVideoDevResList = ExAllocatePool(NonPagedPool, ResourceListSz)) 
+      == NULL ){
+    DbgPrint("couldn't allocate pool\n");
+    ObDereferenceObject(&FileObject);
+    return  STATUS_UNSUCCESSFUL;
+  }
+
+  while( (NtStatus = IoGetDeviceProperty(pVideoDeviceObject, 
+			      DevicePropertyBootConfigurationTranslated,
+			      ResourceListSz, (PVOID)pVideoDevResList, 
+			      &ResourceListNeededSz))
+	 == STATUS_BUFFER_TOO_SMALL ){
+    ExFreePool(pVideoDevResList);
+    if( (pVideoDevResList = ExAllocatePool(NonPagedPool, 
+					  ResourceListNeededSz)) 
+	== NULL ){
+      DbgPrint("couldn't reallocate pool\n");
+      ObDereferenceObject(&FileObject);
+      return  STATUS_UNSUCCESSFUL;
+    }
+  }
+  if(!NT_SUCCESS(NtStatus)){
+	DbgPrint("couldn't get device property\n");
+ObDereferenceObject(&FileObject);
+	return STATUS_UNSUCCESSFUL;
+  }
+  /* have you seen the movie called Inception ? here it goes */
+  DbgPrint("full list starting\n");
+  pFullResourceDescriptor = (PCM_FULL_RESOURCE_DESCRIPTOR) pVideoDevResList->List;
+  for(i = 0; i <  pVideoDevResList->Count; i++,
+	pFullResourceDescriptor = 
+	(PCM_FULL_RESOURCE_DESCRIPTOR)
+	(pFullResourceDescriptor->PartialResourceList.PartialDescriptors +
+	 pFullResourceDescriptor->PartialResourceList.Count)){
+    /* we need to go deeper (c) */
+    PartialResourceList = pFullResourceDescriptor->PartialResourceList;
+    pPartialResourceDescriptor = PartialResourceList.PartialDescriptors;
+    for(j = 0; j < PartialResourceList.Count; j++,
+	  pPartialResourceDescriptor = 
+	  (PCM_PARTIAL_RESOURCE_DESCRIPTOR)
+	  (&PartialResourceList + sizeof(CM_PARTIAL_RESOURCE_LIST)
+	   + j*sizeof(CM_PARTIAL_RESOURCE_DESCRIPTOR))){
+      /* Guess number two: framebuffer is the biggest of smallest */
+      if( (pPartialResourceDescriptor->Type == CmResourceTypeMemory)
+	  &&(FBSz < pPartialResourceDescriptor->u.Memory.Length)){
+	paddr = pPartialResourceDescriptor->u.Memory.Start;
+	FBSz = pPartialResourceDescriptor->u.Memory.Length;
+      }
+    }
+  }
+  DbgPrint("Guessed framebuffer address %p\r\n size %i", paddr.QuadPart
+	   , FBSz);
+  vaddr = MmMapIoSpace(paddr, FBSz, MmNonCached);
+  DbgPrint("FB mapped @ virt addr %p", vaddr);
+  if(!vaddr) NtStatus = STATUS_UNSUCCESSFUL;
+  ExFreePool(pVideoDevResList);
+ObDereferenceObject(&FileObject);
   return NtStatus;
 }
 
 NTSTATUS Read(PDEVICE_OBJECT  DriverObject, PIRP Irp){
-  PVOID FBPhysAddr = 0xd0000000, vaddr = 0;
-  int FBSz = 0x7ffffff, size;
-  PHYSICAL_ADDRESS paddr;
+  
+  int  size;
   PIO_STACK_LOCATION pIoStackIrp = NULL;
   PCHAR pBuffer;
-  int i;
-  char *c;
-
+  NTSTATUS NtStatus = STATUS_SUCCESS;
+ 
   DbgPrint("Read Called \r\n");
+  Irp->IoStatus.Information = 0;
   pIoStackIrp = IoGetCurrentIrpStackLocation(Irp);
   if(pIoStackIrp){
     size = pIoStackIrp->Parameters.Read.Length;
@@ -51,42 +125,45 @@ NTSTATUS Read(PDEVICE_OBJECT  DriverObject, PIRP Irp){
       MmGetSystemAddressForMdlSafe(Irp->MdlAddress, NormalPagePriority);
     DbgPrint("User buffer @ virt addr %p\n", pBuffer);
     if(pBuffer){
-      paddr.QuadPart = FBPhysAddr;
-      vaddr = MmMapIoSpace(paddr, size, MmNonCached);
-      DbgPrint("FB mapped @ virt addr %p", vaddr);
       if(vaddr){
 	DbgPrint("Moving %i bytes from %p to %p\n", size, vaddr, pBuffer);
-	_asm cli;
-	RtlMoveMemory(pBuffer, vaddr, size);
-	_asm sti;
-	MmUnmapIoSpace(vaddr, size);
-	DbgPrint("FB unmapped\n");
+	RtlCopyMemory(pBuffer, vaddr, size);
+	Irp->IoStatus.Information = size;
       }
       else{
 	DbgPrint("vaddr = NULL\r\n");
-	IoCompleteRequest(Irp, IO_NO_INCREMENT);
-	return STATUS_UNSUCCESSFUL;
+	NtStatus = STATUS_UNSUCCESSFUL;
       }
     }
+    else{
+	DbgPrint("vaddr = NULL\r\n");
+	NtStatus = STATUS_UNSUCCESSFUL;
+    }
   }
+  else{
+	DbgPrint("vaddr = NULL\r\n");
+	NtStatus = STATUS_UNSUCCESSFUL;
+  }
+
   DbgPrint("Move successful!\n");
+  Irp->IoStatus.Status = NtStatus;
   IoCompleteRequest(Irp, IO_NO_INCREMENT);
-  return STATUS_SUCCESS;
+  return NtStatus;
 }
 
 NTSTATUS Create(PDEVICE_OBJECT  DriverObject, PIRP Irp){
   DbgPrint("Create called\r\n");
-  return STATUS_SUCCESS;
+  return STATUS_SUCCESS; //STATUS_NOT_IMPLEMENTED ???
 }
 
 NTSTATUS Close(PDEVICE_OBJECT  DriverObject, PIRP Irp){
   DbgPrint("Close Called\r\n");
-  return STATUS_SUCCESS;
+  return STATUS_SUCCESS; //STATUS_NOT_IMPLEMENTED ???
 }
 
 NTSTATUS NotImplemented(PDEVICE_OBJECT  DriverObject, PIRP Irp){
   DbgPrint("NotImplemented called\r\n");
-  return STATUS_SUCCESS;
+  return STATUS_SUCCESS; //STATUS_NOT_IMPLEMENTED ???
 }
 
 void Dtor(PDRIVER_OBJECT  DriverObject){
@@ -95,4 +172,6 @@ void Dtor(PDRIVER_OBJECT  DriverObject){
   RtlInitUnicodeString(&usDosDeviceName, L"\\DosDevices\\Example");
   IoDeleteSymbolicLink(&usDosDeviceName);
   IoDeleteDevice(DriverObject->DeviceObject);
+  MmUnmapIoSpace(vaddr, FBSz);
+  DbgPrint("FB unmapped\n");
 }
