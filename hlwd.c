2f1ba7ca9a28584b7cac3a57b7968c98abe0ea10
Judge_Dredd <the.guard@mail.ru>
2013-10-21 18:43:28 +0400

    added iocontrol to receive fbaddr

--- hlwd.c
+++ hlwd.c
@@ -1,12 +1,17 @@
 #include <ntddk.h>
 #include <Wdm.h>
 
+#define IOCTL_EEYE_INITFB CTL_CODE(FILE_DEVICE_UNKNOWN, 0x800, METHOD_IN_DIRECT,
+FILE_ANY_ACCESS)
+
 NTSTATUS Read(PDEVICE_OBJECT, PIRP);
 NTSTATUS Create(PDEVICE_OBJECT, PIRP);
 NTSTATUS Close(PDEVICE_OBJECT, PIRP);
+NTSTATUS HandleIOCTL(PDEVICE_OBJECT, PIRP)
 NTSTATUS NotImplemented(PDEVICE_OBJECT, PIRP);
 void Dtor(PDRIVER_OBJECT );
-
+int FBPhysAddr = 0, FBSz = 0;
+PVOID vaddr = 0;
 NTSTATUS DriverEntry(PDRIVER_OBJECT pDriverObject, PUNICODE_STRING pRegistryPath){
   NTSTATUS NtStatus = STATUS_SUCCESS;
   PDEVICE_OBJECT pDeviceObject = NULL;
@@ -27,6 +32,7 @@ NTSTATUS DriverEntry(PDRIVER_OBJECT pDriverObject, PUNICODE_STRING pRegistryPath
   pDriverObject->MajorFunction[IRP_MJ_CLOSE] = &Close;
   pDriverObject->MajorFunction[IRP_MJ_CREATE] = &Create;
   pDriverObject->MajorFunction[IRP_MJ_READ] = &Read;
+  pDriverObject->MajorFunction[IRP_MJ_DEVICE_CONTROL] = &HandleIOCTL;
   pDriverObject->DriverUnload = &Dtor;
   pDeviceObject->Flags |= DO_DIRECT_IO;
   pDeviceObject->Flags &= (~DO_DEVICE_INITIALIZING);
@@ -34,9 +40,7 @@ NTSTATUS DriverEntry(PDRIVER_OBJECT pDriverObject, PUNICODE_STRING pRegistryPath
 }
 
 NTSTATUS Read(PDEVICE_OBJECT  DriverObject, PIRP Irp){
-  PVOID FBPhysAddr = 0xd0000000, vaddr = 0;
-  int FBSz = 0x7ffffff, size;
-  PHYSICAL_ADDRESS paddr;
+  int  size;
   PIO_STACK_LOCATION pIoStackIrp = NULL;
   PCHAR pBuffer;
   int i;
@@ -50,28 +54,44 @@ NTSTATUS Read(PDEVICE_OBJECT  DriverObject, PIRP Irp){
     pBuffer =
       MmGetSystemAddressForMdlSafe(Irp->MdlAddress, NormalPagePriority);
     DbgPrint("User buffer @ virt addr %p\n", pBuffer);
-    if(pBuffer){
-      paddr.QuadPart = FBPhysAddr;
-      vaddr = MmMapIoSpace(paddr, size, MmNonCached);
-      DbgPrint("FB mapped @ virt addr %p", vaddr);
-      if(vaddr){
-	DbgPrint("Moving %i bytes from %p to %p\n", size, vaddr, pBuffer);
-	_asm cli;
-	RtlMoveMemory(pBuffer, vaddr, size);
-	_asm sti;
-	MmUnmapIoSpace(vaddr, size);
-	DbgPrint("FB unmapped\n");
-      }
-      else{
-	DbgPrint("vaddr = NULL\r\n");
-	IoCompleteRequest(Irp, IO_NO_INCREMENT);
-	return STATUS_UNSUCCESSFUL;
-      }
+    if( pBuffer && vaddr ){
+      DbgPrint("Moving %i bytes from %p to %p\n", size, vaddr, pBuffer);
+      _asm cli;
+      RtlMoveMemory(pBuffer, vaddr, size);
+      _asm sti;
+    }
+    else{
+      DbgPrint("vaddr or PBuffer = NULL\r\n");
+      IoCompleteRequest(Irp, IO_NO_INCREMENT);
+      return STATUS_UNSUCCESSFUL;
     }
   }
   DbgPrint("Move successful!\n");
   IoCompleteRequest(Irp, IO_NO_INCREMENT);
   return STATUS_SUCCESS;
+}
+
+NTSTATUS HandleIOCTL(PDEVICE_OBJECT  DriverObject, PIRP Irp){
+  NTSTATUS status = STATUS_UNSUCCESSFUL;
+  PIO_STACK_LOCATION pIoStackIrp = IoGetCurrentIrpStackLocation(Irp);
+  int *payload;
+  PHYSICAL_ADDRESS paddr;
+  if( (pIoStackIrp->
+       Parameters.DeviceIoControl.IoControlCode == IOCTL_EEYE_INITFB)
+      && (pIoStackIrp->
+	  Parameters.DeviceIoControl.OutputBufferLength == 2*sizeof(int))
+      && (Irp->AssociatedIrp.SystemBuffer) ){
+    payload = (int *)Irp->AssociatedIrp.SystemBuffer;
+    FBPhysAddr = payload[0];
+    FBSz = payload[1];
+    paddr.QuadPart = FBPhysAddr;
+    vaddr = MmMapIoSpace(paddr, FBSz, MmNonCached);
+    DbgPrint("FBPhysAddr: %p,\nPBSz: 0x%x,\nFB mapped @ virt addr %p\n", 
+	     FBPhysAddr, FBSz, vaddr);
+    status = STATUS_SUCCESS;
+  }
+  else status = STATUS_INVALID_DEVICE_REQUEST;
+  return status;
 }
 
 NTSTATUS Create(PDEVICE_OBJECT  DriverObject, PIRP Irp){
@@ -80,6 +100,11 @@ NTSTATUS Create(PDEVICE_OBJECT  DriverObject, PIRP Irp){
 }
 
 NTSTATUS Close(PDEVICE_OBJECT  DriverObject, PIRP Irp){
+  if(vaddr){ 
+    MmUnmapIoSpace(vaddr, FBSz);
+    vaddr = 0;
+  }
+  DbgPrint("FB unmapped\n");
   DbgPrint("Close Called\r\n");
   return STATUS_SUCCESS;
 }
@@ -92,6 +117,8 @@ NTSTATUS NotImplemented(PDEVICE_OBJECT  DriverObject, PIRP Irp){
 void Dtor(PDRIVER_OBJECT  DriverObject){
   UNICODE_STRING usDosDeviceName;
   DbgPrint("Dtor Called \n");
+  if(vaddr) MmUnmapIoSpace(vaddr, FBSz);
+  DbgPrint("FB unmapped\n");
   RtlInitUnicodeString(&usDosDeviceName, L"\\DosDevices\\Example");
   IoDeleteSymbolicLink(&usDosDeviceName);
   IoDeleteDevice(DriverObject->DeviceObject);
